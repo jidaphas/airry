6ff282aeea0f3e72eb96d23fc4e381eb64d9e8c8
jidaphas <101492491+jidaphas@users.noreply.github.com>
2022-05-27 15:00:44 +0700

    ject

---
+++ src/page/home.js
@@ -0,0 +1,13 @@
+import Bar from '../componant/bar';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+const home = () => {
+    return (
+    < div className = 'home' >
+        <h1> Numerical Project</h1>
+        <h4> น.ส.จิดาภา สำลีทอง</h4>
+        <h4> ภาควิทยาการคอมพิวเตอร์</h4>
+    </div >
+    )
+};
+export default home;
